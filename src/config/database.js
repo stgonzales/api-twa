31df7e3a12aba93bea3d32a036add1cd07586148
Stephen Goncalves <stiefo.o@gmail.com>
2020-05-07 18:02:56 +0100

    Hide sensitive info

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
+
 module.exports = {
-    dialect: 'mssql',
-    host: 'localhost',
-    username: 'sa',
-    password: '*4072300Dev',
-    database: 'TWA_Dev',
+    dialect: process.env.DIALECT,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
     define:{
         timestamps: true,
         underscored: true
