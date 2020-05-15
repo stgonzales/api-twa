0d3e5de7dab3a2a5ad0a523ea7218a9b96aee120
Stephen Goncalves <stiefo.o@gmail.com>
2020-05-15 12:38:48 +0100

    Add Update User route

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,9 +2,15 @@ const User = require('../models/User')
 
 module.exports = {
     async index(req, res){
-        const users = await User.findAll()
+        
+        setTimeout( async function(){ 
+            const users = await User.findAll()
+            return res.json(users);
+        }, 1000);
+        
+        //const users = await User.findAll()
 
-        return res.json(users);
+        
     },
     
     async store(req, res){
@@ -13,5 +19,41 @@ module.exports = {
         const user = await User.create({name, email})
 
         return res.json(user);
+    },
+
+    async update(req, res){
+        const {id, name, email} = req.body
+
+        await User.update({
+            name,
+            email
+        },
+        {
+            where: {id}
+        })
+            .then( count => {
+                console.log(`Rows updated ${count}`)
+                return res.status(200).json({message:`Updated successfully, Rows updated ${count}`})
+            })
+            .catch(err => {return res.status(500).json({message: `Internal server error (SQL Server API): ${err}`})})
+    },
+
+    async exclude(req, res){
+                
+        await User.destroy({
+            where:{
+                id: req.params.id
+            }
+        })
+        .then(function(rowDeleted){
+            if(rowDeleted === 1){
+                return res.status(200).json({message:"Deleted successfully"})
+            }else{
+                return res.status(404).json({message:"record not found"})
+            }
+        })
+        .catch(function(err) {
+            return res.status(500).json(error)
+        })
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,16 @@ const UserController = require('./controllers/UserController')
 
 const routes = express.Router()
 
+//USER-ROUTES
 routes.get('/users', UserController.index)
 
 routes.post('/new-user', UserController.store)
+
+routes.put('/updt-user', UserController.update)
+
+routes.delete('/delete-user/:id', UserController.exclude)
+
+//TICKET-ROUTES
+//TO-DO
 
 module.exports = routes;
