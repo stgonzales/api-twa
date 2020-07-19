ee6fca9be12049bee18face1b1552c7a3ca763ee
Stephen Goncalves <stiefo.o@gmail.com>
2020-07-19 19:33:39 +0100

    Implemented Nginx for container orchestration

--- src/routes.js
+++ src/routes.js
@@ -5,19 +5,19 @@ const CustomerController = require('./controllers/CustomerController')
 const routes = express.Router()
 
 //USER-ROUTES
-routes.get('/users', UserController.index)
+routes.get('/twa/users', UserController.index)
 
-routes.post('/new-user', UserController.store)
+routes.post('/twa/new-user', UserController.store)
 
-routes.put('/updt-user', UserController.update)
+routes.put('/twa/updt-user', UserController.update)
 
 
 //CUSTOMER-ROUTES
-routes.get('/customers', CustomerController.index)
+routes.get('/twa/customers', CustomerController.index)
 
-routes.post('/new-customer', CustomerController.store)
+routes.post('/twa/new-customer', CustomerController.store)
 
-routes.put('/updt-customer', CustomerController.update)
+routes.put('/twa/updt-customer', CustomerController.update)
 
 
 //TO-DO
